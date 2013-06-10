d567f39acc87c6ca5457ae9dd4c9e14ba415ba46
Denis Ivanov <visible.h4x@gmail.com>
2013-06-10 16:15:07 -0700

    Extracting #existing_method

--- lib/durable_decorator/base.rb
+++ lib/durable_decorator/base.rb
@@ -4,14 +4,7 @@ module DurableDecorator
       REDEFINITIONS = {}
 
       def redefine clazz, method_name, &block
-        begin
-          old_method = clazz.instance_method(method_name)
-        rescue NameError => e
-          raise UndefinedMethodError, "#{clazz}##{method_name} is not defined."
-        end
-
-        raise BadArityError, "Attempting to override #{clazz}##{method_name} with incorrect arity." unless block.arity == old_method.arity
-        #store_redefinition(clazz, method_name, old_method)
+        old_method = existing_method clazz, method_name, &block
 
         clazz.class_eval do
           instance_variable_set(previous_method_name(method_name, true), instance_method(method_name))
@@ -22,6 +15,18 @@ module DurableDecorator
         base = "old_#{name}_method"
         base = "@#{base}" if inst_var
         base.to_sym
+      end
+
+      def existing_method clazz, method_name, &block
+        begin
+          old_method = clazz.instance_method(method_name)
+        rescue NameError => e
+          raise UndefinedMethodError, "#{clazz}##{method_name} is not defined."
+        end
+
+        raise BadArityError, "Attempting to override #{clazz}##{method_name} with incorrect arity." unless block.arity == old_method.arity
+
+        old_method
       end
      # def store_redefinition clazz, name, old_method
      #   class_index = REDEFINITIONS[clazz.to_sym] ||= {}
