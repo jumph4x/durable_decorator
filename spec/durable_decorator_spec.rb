7c852d6ef6e382bf46726b56636ddb5e24d6df8b
Denis Ivanov <visible@jumph4x.net>
2014-02-17 20:07:35 -0800

    Refactor spec suite to use a namespace and private/protected method lookup

--- spec/durable_decorator_spec.rb
+++ spec/durable_decorator_spec.rb
@@ -4,22 +4,48 @@ describe DurableDecorator::Base do
 
   context 'with classes' do
   # Spec uses ./example_class.rb
+    context 'for protected methods' do
+      it 'finds them' do
+        Namespace::ExampleClass.class_eval do
+          durably_decorate :protected_method do
+            original_protected_method + " decorated!"
+          end
+        end
+
+        instance = Namespace::ExampleClass.new
+        instance.protected_method.should == 'protected method decorated!'
+      end
+    end
+
+    context 'for private methods' do
+      it 'finds them' do
+        Namespace::ExampleClass.class_eval do
+          durably_decorate :private_method do
+            original_private_method + " decorated!"
+          end
+        end
+
+        instance = Namespace::ExampleClass.new
+        instance.private_method.should == 'private method decorated!'
+      end
+    end
+
     context 'for existing instance methods' do
       it 'guarantees access to #original_method' do
-        ExampleClass.class_eval do
+        Namespace::ExampleClass.class_eval do
           durably_decorate :no_param_method do
             original_no_param_method + " and a new string"
           end
         end
 
-        instance = ExampleClass.new
+        instance = Namespace::ExampleClass.new
         instance.no_param_method.should == 'original and a new string'
       end
 
       context 'with incorrect arity' do
         it 'throws an error' do
           lambda{
-            ExampleClass.class_eval do
+            Namespace::ExampleClass.class_eval do
               durably_decorate(:no_param_method){|a,b| }
             end
           }.should raise_error(DurableDecorator::BadArityError)
@@ -28,13 +54,13 @@ describe DurableDecorator::Base do
 
       context 'for methods with parameters' do
         it 'guarantees access to #original_method' do
-          ExampleClass.class_eval do
+          Namespace::ExampleClass.class_eval do
             durably_decorate :one_param_method do |another_string|
               "#{original_one_param_method('check')} and #{another_string}"
             end
           end
 
-          instance = ExampleClass.new
+          instance = Namespace::ExampleClass.new
           instance.one_param_method("here we go").should == 'original: check and here we go'
         end
       end
@@ -42,19 +68,19 @@ describe DurableDecorator::Base do
       context 'for re-run decorations' do
         it 'does not create a new redefinition' do
           2.times {
-            ExampleClass.class_eval do
+            Namespace::ExampleClass.class_eval do
               durably_decorate :no_param_method do
                 original_no_param_method + " and a new string"
               end
             end
           }
-          DurableDecorator::Base.definitions['ExampleClass#no_param_method'].size.should == 2 # original definition and one deoration
+          DurableDecorator::Base.definitions['Namespace::ExampleClass#no_param_method'].size.should == 2 # original definition and one deoration
         end
       end
 
       context 'for double decorations' do
         before do
-          ExampleClass.class_eval do
+          Namespace::ExampleClass.class_eval do
             durably_decorate :one_param_method do |another_string|
               "#{_935888f04d9e132be458591d5755cb8131fec457_one_param_method('check')} and #{another_string}"
             end
@@ -62,7 +88,7 @@ describe DurableDecorator::Base do
         end
 
         it 'works with explicit method version invocation' do
-          ExampleClass.class_eval do
+          Namespace::ExampleClass.class_eval do
             durably_decorate :one_param_method do |boolean|
               if boolean
                 original_one_param_method("older")
@@ -72,26 +98,26 @@ describe DurableDecorator::Base do
             end
           end
 
-          instance = ExampleClass.new
+          instance = Namespace::ExampleClass.new
           instance.one_param_method(true).should == "original: older"
           instance.one_param_method(false).should == 'original: check and newer'
         end
 
         it 'work with short explicit method version invocation' do
-          instance = ExampleClass.new
+          instance = Namespace::ExampleClass.new
           instance._9358_one_param_method('').should == "original: "
           instance._935888_one_param_method('').should == "original: "
         end
 
         it 'maintains history of decorations' do
-          DurableDecorator::Base.definitions['ExampleClass#one_param_method'].size.should == 2
+          DurableDecorator::Base.definitions['Namespace::ExampleClass#one_param_method'].size.should == 2
         end
       end
 
       context 'for strict definitions' do
         context 'with the correct SHA' do
           it 'guarantees access to #method_original' do
-            ExampleClass.class_eval do
+            Namespace::ExampleClass.class_eval do
               meta = {
                 :mode => 'strict',
                 :sha => 'd54f9c7ea2038fac0ae2ff9af49c56f35761725d'
@@ -101,7 +127,7 @@ describe DurableDecorator::Base do
               end
             end
 
-            instance = ExampleClass.new
+            instance = Namespace::ExampleClass.new
             instance.no_param_method.should == "original and a new string"
           end
         end
@@ -109,7 +135,7 @@ describe DurableDecorator::Base do
         context 'with the wrong SHA' do
           it 'raises an error' do
             lambda{
-              ExampleClass.class_eval do
+              Namespace::ExampleClass.class_eval do
                 meta = {
                   :mode => 'strict',
                   :sha => '1234wrong'
@@ -125,7 +151,7 @@ describe DurableDecorator::Base do
         context 'for an invalid config' do
           it 'raises an error' do
             lambda{
-              ExampleClass.class_eval do
+              Namespace::ExampleClass.class_eval do
                 meta = {
                   :mode => 'strict'
                 }
@@ -141,7 +167,7 @@ describe DurableDecorator::Base do
       context 'for soft definitions' do
         context 'with the correct SHA' do
           it 'guarantees access to #original_method' do
-            ExampleClass.class_eval do
+            Namespace::ExampleClass.class_eval do
               meta = {
                 :mode => 'soft',
                 :sha => 'd54f9c7ea2038fac0ae2ff9af49c56f35761725d'
@@ -151,14 +177,14 @@ describe DurableDecorator::Base do
               end
             end
 
-            instance = ExampleClass.new
+            instance = Namespace::ExampleClass.new
             instance.no_param_method.should == "original and a new string"
           end
         end
 
         context 'with the wrong SHA' do
           it 'logs a warning but does not raise an error' do
-            ExampleClass.class_eval do
+            Namespace::ExampleClass.class_eval do
               meta = {
                 :mode => 'soft',
                 :sha => '1234wrong'
@@ -173,7 +199,7 @@ describe DurableDecorator::Base do
 
         context 'when lacking SHA' do
           it 'prints the SHA suggestion' do
-            ExampleClass.class_eval do
+            Namespace::ExampleClass.class_eval do
               durably_decorate :no_param_method do
                 no_param_method_original + " and a new string"
               end
@@ -185,7 +211,7 @@ describe DurableDecorator::Base do
         context 'for an invalid config' do
           it 'raises an error' do
             lambda{
-              ExampleClass.class_eval do
+              Namespace::ExampleClass.class_eval do
                 meta = {
                   :mode => 'strict'
                 }
@@ -202,7 +228,7 @@ describe DurableDecorator::Base do
     context 'for methods not yet defined' do
       it 'throws an error' do
         lambda{
-          ExampleClass.class_eval do
+          Namespace::ExampleClass.class_eval do
             durably_decorate(:integer_method){ }
           end
         }.should raise_error(DurableDecorator::UndefinedMethodError)
@@ -211,19 +237,19 @@ describe DurableDecorator::Base do
 
     context 'for existing class methods' do
       it 'guarantees access to ::original_method' do
-        ExampleClass.class_eval do
+        Namespace::ExampleClass.class_eval do
           durably_decorate_singleton :clazz_level do
             original_clazz_level + " and a new string"
           end
         end
 
-        ExampleClass.clazz_level.should == 'original and a new string'
+        Namespace::ExampleClass.clazz_level.should == 'original and a new string'
       end
 
       context 'with incorrect arity' do
         it 'throws an error' do
           lambda{
-            ExampleClass.class_eval do
+            Namespace::ExampleClass.class_eval do
               durably_decorate_singleton(:clazz_level){|a,b| }
             end
           }.should raise_error(DurableDecorator::BadArityError)
@@ -232,13 +258,13 @@ describe DurableDecorator::Base do
 
       context 'for methods with parameters' do
         it 'guarantees access to ::original_method' do
-          ExampleClass.class_eval do
+          Namespace::ExampleClass.class_eval do
             durably_decorate_singleton :clazz_level_paramed do |another_string|
               "#{original_clazz_level_paramed('check')} and #{another_string}"
             end
           end
 
-          ExampleClass.clazz_level_paramed("here we go").should == 'original: check and here we go'
+          Namespace::ExampleClass.clazz_level_paramed("here we go").should == 'original: check and here we go'
         end
       end
     end
@@ -246,7 +272,7 @@ describe DurableDecorator::Base do
     context 'for methods not yet defined' do
       it 'throws an error' do
         lambda{
-          ExampleClass.class_eval do
+          Namespace::ExampleClass.class_eval do
             durably_decorate_singleton(:integer_method){ }
           end
         }.should raise_error(DurableDecorator::UndefinedMethodError)
@@ -303,14 +329,14 @@ describe DurableDecorator::Base do
 
       context 'when the target is an instance method' do
         it 'should return the sha' do
-          DurableDecorator::Base.determine_sha('ExampleClass#no_param_method').should ==
+          DurableDecorator::Base.determine_sha('Namespace::ExampleClass#no_param_method').should ==
             'd54f9c7ea2038fac0ae2ff9af49c56f35761725d'
         end
       end
 
       context 'when the target is a class method' do
         it 'should return the sha' do
-          DurableDecorator::Base.determine_sha('ExampleClass.clazz_level').should ==
+          DurableDecorator::Base.determine_sha('Namespace::ExampleClass.clazz_level').should ==
             'f6490bec1af021697ed8e5990f0d1db3976f065f'
         end
       end
--- spec/example_class.rb
+++ spec/example_class.rb
@@ -1,17 +1,29 @@
-class ExampleClass
-  def no_param_method
-    "original"
-  end
+module Namespace
+  class ExampleClass
+    def no_param_method
+      "original"
+    end
 
-  def one_param_method param
-    "original: #{param}"
-  end
+    def one_param_method param
+      "original: #{param}"
+    end
 
-  def self.clazz_level
-    "original"
-  end
+    def self.clazz_level
+      "original"
+    end
 
-  def self.clazz_level_paramed param
-    "original: #{param}"
+    def self.clazz_level_paramed param
+      "original: #{param}"
+    end
+
+    private
+    def private_method
+      'private method'
+    end
+
+    protected
+    def protected_method
+      'protected method'
+    end
   end
 end
